f8bc3422700a1abf0c8c53b7daf07add450ed669
karan-lagalwar <lagalwar007@gmail.com>
2021-10-08 20:18:33 +0530

    save changes: used container widget in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Learning App',
-            style: TextStyle(color: Colors.grey[200]),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.teal,
+            body: Container(
+              width: 200.0,
+              height: 200.0,
+              margin: EdgeInsets.only(left: 80.00, top: 30.00),
+              padding: EdgeInsets.all(10.00),
+            // color: Colors.white,
+              child: Text(
+                  'Hello World',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.tealAccent,
+                  letterSpacing: 2.0,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w800
+                ),
+              ),
+            ),
           ),
-        ),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: Image(
-          image: AssetImage('images/diamond.png'),
-        ),
-      ),
-    )),
-  );
+        )
+    );
+  }
 }
