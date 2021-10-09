a838daafa5678fb64cf45a94d71a4ae9372b0d3a
karan-lagalwar <lagalwar007@gmail.com>
2021-10-09 19:58:16 +0530

    save changes: for row-columns widget and there properties

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,110 @@ class MyApp extends StatelessWidget {
         home: SafeArea(
           child: Scaffold(
             backgroundColor: Colors.teal,
-            body: Container(
-              width: 200.0,
-              height: 200.0,
-              margin: EdgeInsets.only(left: 80.00, top: 30.00),
-              padding: EdgeInsets.all(10.00),
-            // color: Colors.white,
-              child: Text(
-                  'Hello World',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.tealAccent,
-                  letterSpacing: 2.0,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w800
+            body: Column(
+              //crossAxisAlignment: CrossAxisAlignment.end,
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  //width: double.infinity,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                        height: 50.0,
+                        // margin: EdgeInsets.only(left: 80.00, top: 30.00),
+                        // padding: EdgeInsets.all(5.00),
+                        color: Colors.blueGrey,
+                        child: Center(
+                          child: Text(
+                            'Row 1',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white,
+                                letterSpacing: 2.0,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.w800
+                            ),
+                          ),
+                        )
+                    ),
+                    Container(
+                        height: 50.0,
+                        // margin: EdgeInsets.only(left: 80.00, top: 30.00),
+                        // padding: EdgeInsets.all(5.00),
+                        color: Colors.indigo,
+                        child: Center(
+                          child: Text(
+                            'Row 2',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.orangeAccent,
+                                letterSpacing: 2.0,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.w800
+                            ),
+                          ),
+                        )
+                    ),
+                    Container(
+                        height: 50.0,
+                        // margin: EdgeInsets.only(left: 80.00, top: 30.00),
+                        // padding: EdgeInsets.all(5.00),
+                        color: Colors.purple,
+                        child: Center(
+                          child: Text(
+                            'Row 3',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.lightGreen,
+                                letterSpacing: 2.0,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.w800
+                            ),
+                          ),
+                        )
+                    )
+                  ],
                 ),
-              ),
+                Container(
+                  //width: 50.0,
+                  height: 50.0,
+                  // margin: EdgeInsets.only(left: 80.00, top: 30.00),
+                  // padding: EdgeInsets.all(5.00),
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      'Container 2',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w800
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //width: 50.0,
+                  height: 50.0,
+                  // margin: EdgeInsets.only(left: 80.00, top: 30.00),
+                  // padding: EdgeInsets.all(5.00),
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'Container 3',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w800
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         )
