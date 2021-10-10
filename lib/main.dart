d019499334448a2bcf6b7f774079fe481119fee9
karan-lagalwar <lagalwar007@gmail.com>
2021-10-10 14:51:01 +0530

    save changes: adding icons and fonts and create MI CARD UI Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,76 @@ class MyApp extends StatelessWidget {
         home: SafeArea(
           child: Scaffold(
             backgroundColor: Colors.teal,
-            body: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            body: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/K.png'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.green,
-                    )
-                  ],
+                Text(
+                  'Karan S',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  'SENIOR DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5
+                  ),
                 ),
                 Container(
-                  width: 100.0,
-                  color: Colors.blue,
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(horizontal: 25.00, vertical: 20.00),
+                  padding: EdgeInsets.all(10.00),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+91 123 456 789',
+                        style: TextStyle(
+                          color: Colors.teal.shade700
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(horizontal: 25.00, vertical: 10.00),
+                  padding: EdgeInsets.all(10.00),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'karan.lagalwar@outlook.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade700
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
